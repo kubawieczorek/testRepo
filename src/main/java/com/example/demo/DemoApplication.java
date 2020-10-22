50d34dbc7c59be3918fcf2129f8cd0fd913cd491
jakub.wieczorek <jwieczorek@future-processing.com>
2020-10-22 20:20:38 +0200

    new variable on branch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         String abcd = "abc";
+        String specialBranchVariable = "123";
         SpringApplication.run(DemoApplication.class, args);
     }
 
