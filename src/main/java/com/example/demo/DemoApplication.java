b76cec873a0335c51b104caac7b25d35f71f16f4
jakub.wieczorek <jwieczorek@future-processing.com>
2020-10-22 20:13:08 +0200

    new variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
+        String k = "aa";
         SpringApplication.run(DemoApplication.class, args);
     }
 
